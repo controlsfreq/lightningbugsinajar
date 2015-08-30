524de0ff6e46ee16fce195c60f9eda8c8cd554c5
controlsfreq <controls.freq@gmail.com>
2015-08-29 21:15:16 -0700

    Pin Driver
    * Added pin driver. AVR implementation only supports bank B. Tested
    toggle, assert, and deassert functions.

--- Makefile
+++ Makefile
@@ -29,10 +29,12 @@ TARGET = lbj.hex
 
 ### Source Files ###
 APP_SOURCES = $(SOURCE_DIR)/main.c \
+              $(SOURCE_DIR)/pin_avr.c \
               $(SOURCE_DIR)/controller.c \
               $(SOURCE_DIR)/bsp.c
 
 APP_HEADERS = $(HEADER_DIR)/controller.h \
+              $(HEADER_DIR)/pin.h \
               $(HEADER_DIR)/bsp.h
 
 APP_OBJECTS = $(addprefix $(BUILD_DIR)/,$(notdir $(APP_SOURCES:.c=.o)))
--- source/main.c
+++ source/main.c
@@ -6,13 +6,13 @@
  * @copyright
  * {
  *     Copyright 2015 Liam Bucci
- *            
+ *
  *     Licensed under the Apache License, Version 2.0 (the "License");
  *     you may not use this file except in compliance with the License.
  *     You may obtain a copy of the License at
- *     
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  *     Unless required by applicable law or agreed to in writing, software
  *     distributed under the License is distributed on an "AS IS" BASIS,
  *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,6 +26,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "pin.h"
 #include "controller.h"
 
 
@@ -34,13 +35,17 @@ int main( int argc, char *argv[] )
 	//char major_version = LBJ_MAJOR_VERSION;
 	//char minor_version = LBJ_MINOR_VERSION;
 	//char patch_version = LBJ_PATCH_VERSION;
-	
-	DDRB |= _BV(DDB3); 
-    
-    while(1) 
+
+	pin_t led = { PIN_BANK_B, PIN_NUM_3, false };
+
+    (void)pin_set_mode( &led, PIN_MODE_OUTPUT );
+
+    while(1)
     {
-        PORTB ^= _BV(PB3);
-        _delay_ms(500);
+        (void)pin_assert( &led );
+        _delay_ms(300);
+        (void)pin_deassert( &led );
+        _delay_ms(300);
     }
 
 	return 0;
