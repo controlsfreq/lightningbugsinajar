58b8192d4b51fab611f624a5cefa22a1d3642dda
controlsfreq <controls.freq@gmail.com>
2015-09-16 22:33:07 -0700

    Timer1, Linker Script, Fuse Bits
    * Added general Timer interface.
    * Added AVR timer1 implementation.
    * Added linker script with attiny85 memory settings (avr-gcc uses a
    generic script with inflated memory regions).
    * Added command line options to program fuse bits from Makefile.

--- Makefile
+++ Makefile
@@ -10,13 +10,19 @@ PROG = avrdude
 MKDIR = mkdir -p
 RM    = rm -f
 RMDIR = rm -fr
+TOUCH = touch
+
+### Fuse Bits ###
+LFUSE = 0x62
+HFUSE = 0xDF
+EFUSE = 0xFF
 
 ### Build Flags ###
 CC_FLAGS = -g -mmcu=attiny85 -Wall -Werror -Os -D LBJ_MAJOR_VERSION=$(MAJOR_VERSION) -D LBJ_MINOR_VERSION=$(MINOR_VERSION) -D LBJ_PATCH_VERSION=$(PATCH_VERSION)
-LINK_FLAGS = -g -mmcu=attiny85 -Wl,-Map,$(OUTPUT_DIR)/$(TARGET:.hex=.map)
+LINK_FLAGS = -g -mmcu=attiny85 -Wl,-Map,$(OUTPUT_DIR)/$(TARGET:.hex=.map) -Wl,-T,linker/attiny85.x
 AR_FLAGS = rcs
 OBJCOPY_FLAGS = -j .text -j .data -O ihex
-PROG_FLAGS = -p t85 -c avrispmkII -P usb
+PROG_FLAGS = -p t85 -c avrispmkII -P usb -U lfuse:w:$(LFUSE):m -U hfuse:w:$(HFUSE):m -U efuse:w:$(EFUSE):m
 
 ### Directories ###
 BUILD_DIR = ./build
@@ -32,6 +38,7 @@ APP_SOURCES = $(SOURCE_DIR)/main.c \
               $(SOURCE_DIR)/pin_avr.c \
               $(SOURCE_DIR)/spi_avr.c \
               $(SOURCE_DIR)/tlc591x_avr.c \
+              $(SOURCE_DIR)/avr_timer.c \
               $(SOURCE_DIR)/controller.c \
               $(SOURCE_DIR)/bsp.c
 
@@ -39,6 +46,7 @@ APP_HEADERS = $(HEADER_DIR)/controller.h \
               $(HEADER_DIR)/pin.h \
               $(HEADER_DIR)/spi.h \
               $(HEADER_DIR)/tlc591x.h \
+              $(HEADER_DIR)/timer.h \
               $(HEADER_DIR)/bsp.h
 
 APP_OBJECTS = $(addprefix $(BUILD_DIR)/,$(notdir $(APP_SOURCES:.c=.o)))
@@ -61,10 +69,10 @@ $(BUILD_DIR)/%.o: $(SOURCE_DIR)/%.c
 	$(CC) $(CC_FLAGS) -I$(HEADER_DIR) -MM $< | sed "1 s|^|$(BUILD_DIR)/|" > $(BUILD_DIR)/$*.d
 
 $(BUILD_DIR)/$(TARGET:.hex=.elf): $(APP_OBJECTS)
+	@$(MKDIR) $(OUTPUT_DIR)
 	$(CC) $(LINK_FLAGS) -o $@ $(APP_OBJECTS)
 
 $(OUTPUT_DIR)/$(TARGET): $(BUILD_DIR)/$(TARGET:.hex=.elf)
-	@$(MKDIR) $(OUTPUT_DIR)
 	$(OBJCOPY) $(OBJCOPY_FLAGS) $(BUILD_DIR)/$(TARGET:.hex=.elf) $(OUTPUT_DIR)/$(TARGET)
 
 clean:
--- source/main.c
+++ source/main.c
@@ -24,20 +24,34 @@
 #define F_CPU 1000000UL
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 #include "pin.h"
 #include "spi.h"
 #include "tlc591x.h"
 #include "controller.h"
+#include "timer.h"
 
+volatile uint8_t g_timer_counter = 0;
+
+pin_t debug_pin = {
+    PIN_BANK_B,
+    PIN_NUM_3,
+    false
+};
 
 int main( int argc, char *argv[] )
 {
-    uint8_t output = 0;
 	//char major_version = LBJ_MAJOR_VERSION;
 	//char minor_version = LBJ_MINOR_VERSION;
 	//char patch_version = LBJ_PATCH_VERSION;
+
+    timer_t timer1 = {
+        TIMER_NUM_1,
+        TIMER_PRE_1024,
+        true
+    };
 
     spi_t spi = {
         SPI_BAUD_1M,
@@ -45,22 +59,22 @@ int main( int argc, char *argv[] )
             PIN_BANK_B,
             PIN_NUM_3,
             true
-        },
+        }, // SPI CS
         {
             PIN_BANK_B,
             PIN_NUM_1,
             false
-        },
+        }, // SPI MOSI
         {
             PIN_BANK_B,
             PIN_NUM_0,
             false
-        },
+        }, // SPI MISO
         {
             PIN_BANK_B,
             PIN_NUM_2,
             false
-        }
+        } // SPI SCLK
     };
 
     tlc591x_t tlc = {
@@ -69,16 +83,104 @@ int main( int argc, char *argv[] )
         { PIN_BANK_B, PIN_NUM_3, true }
     };
 
+    (void)timer_init( &timer1 );
+    (void)timer_set_compare( &timer1, 62 );
     (void)tlc591x_init( &tlc );
     (void)tlc591x_enable_output( &tlc );
 
+    sei();
+
+    (void)timer_enable_int( &timer1 );
+    (void)timer_start( &timer1 );
+
     while(1)
     {
-        (void)tlc591x_write_values( &tlc, output );
-        output++;
-        output = output % 16;
-        _delay_ms(500);
+        (void)tlc591x_write_values( &tlc, g_timer_counter );
     }
 
 	return 0;
+}
+
+ISR(TIM1_COMPA_vect)
+{
+    g_timer_counter++;
+    (void)pin_toggle( &debug_pin );
+}
+
+ISR(INT0_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(PCINT0_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(TIM1_OVF_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(TIM0_OVF_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(EE_RDY_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(ANA_COMP_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(ADC_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(TIM1_COMPB_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(TIM0_COMPA_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(TIM0_COMPB_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(WDT_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(USI_START_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
+}
+
+ISR(USI_OVF_vect)
+{
+    (void)pin_assert( &debug_pin );
+    while(1); /* Trap */
 }
